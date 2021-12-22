c3f393734ff86b324bf6fb37b9fd161050227712
hamsame <hamsaaelmi@gmail.com>
2021-12-22 22:46:02 +0000

    Changed html attributes on jsx element to jsx attributes to solve build faiure.

--- pages/about.js
+++ pages/about.js
@@ -13,8 +13,8 @@ export default function About() {
           rel='stylesheet'
           href='https://cdnjs.cloudflare.com/ajax/libs/line-awesome/1.3.0/line-awesome/css/line-awesome.min.css'
           integrity='sha512-vebUliqxrVkBy3gucMhClmyQP9On/HAWQdKDXRaAlb/FKuTbxkjPKUyqVOxAcGwFDka79eTF+YXwfke1h3/wfg=='
-          crossorigin='anonymous'
-          referrerpolicy='no-referrer'
+          crossOrigin='anonymous'
+          referrerPolicy='no-referrer'
         />
       </Head>
 
